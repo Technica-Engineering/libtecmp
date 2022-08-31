44118284dcdb7d0612e7980b4683493432a70ceb
Andreas Amereller <andreas.amereller@technica-engineering.de>
2022-08-31 16:54:23 +0200

    [ANDI-8996/AnAm]: Changed ethertype to respect all VLAN tags.

--- src/tecmp.c
+++ src/tecmp.c
@@ -52,11 +52,12 @@ int tecmp_next(
 
 	uint16_t ether = ntoh16(*((uint16_t*)(raw_frame + 12)));
 
-	// Ethertype mismatch
-	if (ether == 0x8100) {
+	// VLAN ethertypes
+	if ((ether == 0x8100) || (ethertype == 0x88A8) || (ethertype == 0x9100)) {
 		uint32_t skip = ETH_SIZE + 2;
 		return tecmp_nect(raw_frame + skip, raw_frame_size - skip, iterator, header, data);
 	}
+	// Ethertype mismatch
 	if (!(ether == 0x2090 || ether == 0x99FE)) {
 		return EINVAL;
 	}
