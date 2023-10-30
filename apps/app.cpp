b6bd0f3755ae4605f1fbcaf34e308f6b16547a3b
rawiamoalla <90904507+rawiamoalla@users.noreply.github.com>
2023-10-30 14:16:00 +0100

    fix malformed packet when depacketizing a tecmp message (#17)
    ---------
    
    Co-authored-by: rawia.moalla <rawia.moalla}technica-engineering.de>

--- apps/app.cpp
+++ apps/app.cpp
@@ -12,47 +12,6 @@
 using namespace pcpp;
 using namespace std;
 
-
-void print_status(tecmp_header header, uint8_t* data) {
-	switch (header.message_type)
-	{
-		case TECMP_TYPE_CM_STATUS:
-		{
-			tecmp_cm_status cm_status;
-			tecmp_get_cm_status(header, data, &cm_status);
-			auto v = cm_status.vendor;
-			printf("CM %u:\n", cm_status.cm_id);
-			printf("\t Version: v%u.%u.%u\n", v.sw_version_major, v.sw_version_minor, v.sw_version_patch);
-			break;
-		}
-
-		case TECMP_TYPE_BUS_STATUS:
-		{
-			int32_t iterator = 0;
-			tecmp_bus_status bus_status;
-			while (!tecmp_next_bus_status(header, data, &iterator, &bus_status)) {
-				printf("CM %u: %x\n", bus_status.cm_id, bus_status.channel_id);
-				printf("\t Messages Total: %u\n", bus_status.messages_total);
-				printf("\t Errors Total: %u\n", bus_status.errors_total);
-				printf("\t Link Status: %u\n", bus_status.vendor.link_status);
-				printf("\t Link Quality: %u\n", bus_status.vendor.link_quality);
-
-				auto linkup_time = bus_status.vendor.linkup_time;
-				if (linkup_time == 0) {
-					printf("\t Linkup Time: no linkup detected yet\n");
-				}
-				else if (linkup_time == 0xffff) {
-					printf("\t Linkup Time: no linkup detected and timeout occurred\n");
-				}
-				else {
-					printf("\t Linkup Time: %ums\n", bus_status.vendor.linkup_time);
-				}
-			}
-			break;
-		}
-	}
-}
-
 /*
  * Sample app to write packetized TECMP into depacketized TECMP
  * Also removes the TECMP header from Ethernet Data Frames
@@ -106,32 +65,19 @@ int main(int argc, char* argv[]) {
 
 				RawPacket newP;
 				timeval time = tecmp_get_timeval(header);
+				// Capture Module MTU = 1600
+				uint8_t buffer[1600];
 
-				if (header.data_type == TECMP_DATA_ETHERNET)
-				{
-					// Ethernet Frame, write it back to the PCAP file without TECMP header
-					newP = RawPacket(data, header.length, time, false);
-				}
-				else
-				{
-					// Print Status to stdout
-					print_status(header, data);
+				// 14 Ethernet header + TECMP header
+				const uint16_t head_size = 14 + 12;
+				memcpy(buffer, p.getRawData(), head_size);
 
-					// Write back the frame depacketized
-
-					// Capture Module MTU = 1600
-					uint8_t buffer[1600];
-
-					// 14 Ethernet header + TECMP header
-					const uint16_t head_size = 14 + sizeof(tecmp_header);
-					memcpy(buffer, p.getRawData(), head_size);
-
-					// Copy the data header and data back
-					memcpy(buffer + head_size, data, header.length);
+				// Copy the payload header and data
+				const uint16_t hdr_size = 16;
+				memcpy(buffer + head_size, data - hdr_size, header.length + hdr_size);
 
 
-					newP = RawPacket(buffer, header.length + head_size, time, false);
-				}
+				newP = RawPacket(buffer, header.length + head_size + hdr_size, time, false);
 
 				// Write packet back
 				writer.writePacket(newP);
@@ -140,7 +86,6 @@ int main(int argc, char* argv[]) {
 				res = tecmp_next(p.getRawData(), p.getRawDataLen(), &iterator, &header, &data);
 			}
 		}
-	}
 
 	reader->close();
 	writer.close();
